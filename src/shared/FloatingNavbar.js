b0709f840b62d5ae3c378e4f85193911132600a5
masoud <masoudmotahari1380@gmail.com>
2022-08-17 03:17:08 -0700

    complete floating navbar and add mui component

--- src/shared/FloatingNavbar.js
+++ src/shared/FloatingNavbar.js
@@ -1,41 +1,52 @@
-import React, { useState } from "react";
-//import icons from material icon
+import React from "react";
+import { useRouter } from "next/router";
+import Box from "@mui/material/Box";
+import Backdrop from "@mui/material/Backdrop";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
 import CategoryIcon from "@mui/icons-material/Category";
-import PersonIcon from '@mui/icons-material/Person';
+import PersonIcon from "@mui/icons-material/Person";
 import MenuBookIcon from "@mui/icons-material/MenuBook";
 import HomeIcon from "@mui/icons-material/Home";
-import WidgetsIcon from '@mui/icons-material/Widgets';
 
-const FloatingNavbar = () => {
-  
-  const [isOpenNavbar, setIsOpenNavbar] = useState(false);
+const actions = [
+  { icon: <CategoryIcon />, name: "Category", href: "/categories" },
+  { icon: <PersonIcon />, name: "Authors", href: "/authors" },
+  { icon: <MenuBookIcon />, name: "Blogs", href: "/blogs" },
+  { icon: <HomeIcon />, name: "Home", href: "/" },
+];
+
+export default function SpeedDialTooltipOpen() {
+  const router = useRouter();
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <div className=" fixed bottom-5 left-3 bg-dark rounded-full w-10 h-10 flex justify-center flex-col z-[999] items-center ">
-      {/* button for open or close menu */}
-      <button onClick={() => setIsOpenNavbar(!isOpenNavbar)} className="w-10 h-10 rounded-full select-none">
-        <WidgetsIcon className="text-white" />
-      </button>
-      <div
-        className={`absolute bottom-10 w-20 text-center transition-all  duration-500 rounded even:delay-150  ${
-          isOpenNavbar ? "-left-2" : "-left-32"
-        }`}
+    <Box className="fixed bottom-3 right-3 z-[999] ">
+      <Backdrop open={open} />
+      <SpeedDial
+        ariaLabel="SpeedDial tooltip example"
+        sx={{ position: "absolute", bottom: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
       >
-        {/* menu links  */}
-        <div className={`shadow shadow-darkBlue bg-lightBlue rounded my-2 `}>
-          <HomeIcon />
-        </div>
-        <div className={`shadow shadow-darkBlue bg-lightBlue rounded my-2`}>
-          <MenuBookIcon />
-        </div>
-        <div className={`shadow shadow-darkBlue bg-lightBlue rounded my-2`}>
-          <PersonIcon />
-        </div>
-        <div className={`shadow shadow-darkBlue bg-lightBlue rounded my-2`}>
-          <CategoryIcon />
-        </div>
-      </div>
-    </div>
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={() => {
+              handleClose();
+              router.push(action.href);
+            }}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
   );
-};
-
-export default FloatingNavbar;
+}
